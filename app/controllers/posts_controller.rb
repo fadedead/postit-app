c8a4dd047977194679c22bc489f5acc7aaa4169e
rohit_mathew <rohit_mathew@apple.com>
2015-06-08 14:34:44 -0700

    Update untill Lesson Lecture 1 b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,41 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
   def show
-    @post = Post.find(params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
+    @post = Post.new(post_parameters)
+    @post.user = User.first
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
   def edit
   end
-  def update 
+  def update
+    if @post.update(post_parameters)
+      flash[:notice] = "The post was updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
+
+  private 
+
+  def post_parameters
+    params.require(:post).permit(:url, :title, :description)
+  end
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :categories, through: :post_categories
   has_many :post_categories
+
+  validates :title, length: {minimum: 10}
+  validates :url, presence: true
+  validates :description, length: {minimum: 25}
 end
